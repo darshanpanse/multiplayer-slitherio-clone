563115a986f64274b942503c5678136d526c4c2d
darshanpanse <darsh.panse@gmail.com>
2020-05-03 20:41:46 +0000

    MSC-1: Loads assets and tile sprite.

--- public/js/game.js
+++ public/js/game.js
@@ -1,19 +1,35 @@
-var config = {
+const config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
   scene: {
     preload: preload,
     create: create,
     update: update
-  } 
+  }
 };
 
-var game = new Phaser.Game(config);
- 
-function preload() {}
- 
-function create() {}
- 
-function update() {}
+let game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('background', 'asset/tile.png');
+}
+
+function create() {
+  let width = this.physics.world.bounds.width;
+  let height = this.physics.world.bounds.height;
+  this.add.tileSprite(0, 0, width * 2, height * 2, 'background');
+  
+}
+
+function update() {
+
+}
